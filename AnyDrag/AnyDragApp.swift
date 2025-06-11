b5e9793429b6e1024f84a5571b211b3a38852ed2
Ufuk Özen <fenerplayht5@gmail.com>
2025-06-11 05:15:26 +0300

    Initial Commit

---
+++ AnyDrag/AnyDragApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyDragApp.swift
+//  AnyDrag
+//
+//  Created by luckymac on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnyDragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
